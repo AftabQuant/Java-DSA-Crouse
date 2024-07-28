2a0d60152cebc5933a7ed36e0d0e054ea8e30d3b
AftabQuant <iamaftabuddin123@gmail.com>
2024-07-28 20:01:12 +0530

    Add Impotant

---
+++ Tree DS/src/BinaryTree/important.java
@@ -0,0 +1,26 @@
+package BinaryTree;
+
+public class important{
+    static int size(Node root){
+        if(root == null) return 0;
+        return 1+ size(root.left)+size(root.right);
+    }
+    static int level(Node root){
+        if(root == null) return 0;
+        return 1+ Math.max(level(root.left),level(root.right));
+    }
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.right = f;
+        System.out.println(size(a));
+        System.out.println(level(a));
+        System.out.println(height(a));
+    }
+}
