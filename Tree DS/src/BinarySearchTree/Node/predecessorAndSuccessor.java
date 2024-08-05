5c357b666954988300ba20cf5323ae6b625605bc
AftabQuant <iamaftabuddin123@gmail.com>
2024-08-05 23:53:19 +0530

    BSTree

---
+++ Tree DS/src/BinarySearchTree/Node/predecessorAndSuccessor.java
@@ -0,0 +1,41 @@
+package BinarySearchTree.Node;
+import java.util.*;
+public class predecessorAndSuccessor{
+    private static void inorder(Node root, List<Integer> arr ){
+        if(root==null) return;
+        inorder(root.left, arr);
+        arr.add(root.val);
+        inorder(root.right, arr);
+    }
+    static void predecessor(Node root){
+        Node prev = root.left;
+        while(prev.right!=null){
+            prev = prev.right;
+        }
+        System.out.println(prev.val);
+    }
+    public static void main(String[] args) {
+        Node a = new Node(50);
+        Node b = new Node(30);
+        Node c = new Node(70);
+        Node d = new Node(20);
+        Node e = new Node(40);
+        Node f = new Node(60);
+        Node g = new Node(80);
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.left = f; c.right = g;
+        List<Integer> arr = new ArrayList<>();
+        inorder(a,arr);
+        System.out.println(arr);
+        for(int i=0; i<arr.size(); i++){
+            if(arr.get(i)==60){
+                System.out.println("Predecessor : " + arr.get(i-1));
+                if(i==arr.size()-1) return;
+                else System.out.println("successor : " + arr.get(i+1));
+                break;
+            }
+        }
+        predecessor(a);
+    }
+}
---
+++ Tree DS/src/BinarySearchTree/Node/reverseBST.java
@@ -0,0 +1,23 @@
+package BinarySearchTree.Node;
+
+public class reverseBST{
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(5);
+        Node c = new Node(15);
+        Node d = new Node(2);
+        Node e = new Node(8);
+        Node f = new Node(12);
+        Node g = new Node(19);
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.left = f; c.right = g;
+        display(a);
+    }
+    private static void display(Node root){
+        if(root==null) return;
+        display(root.right);
+        System.out.print(root.val+" ");
+        display(root.left);
+    }
+}
