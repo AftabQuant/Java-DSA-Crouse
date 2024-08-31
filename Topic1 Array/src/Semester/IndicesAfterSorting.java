7b8c5620e8c12bd6bd0b70837850e6436b362e6b
AftabQuant <iamaftabuddin123@gmail.com>
2024-09-01 00:32:41 +0530

    Binary Search

---
+++ Topic1 Array/src/Semester/IndicesAfterSorting.java
@@ -0,0 +1,22 @@
+package Semester;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IndicesAfterSorting{
+    public static List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> ans = new ArrayList<>();
+        int lo=0; int hi=nums.length-1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==target) ans.add(i);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] num = {1,2,5,2,3}; int target = 2;
+        List<Integer> ans = targetIndices(num,target);
+        System.out.println(ans);
+    }
+}
