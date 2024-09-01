4a2e1cc8cd1c8c4a3e909f6f6b2a6d6c38ed46f0
AftabQuant <iamaftabuddin123@gmail.com>
2024-09-01 23:49:31 +0530

    Q

--- Heaps And PQ/src/Heap/LastStoneWeight.java
+++ Heaps And PQ/src/Heap/LastStoneWeight.java
@@ -2,7 +2,7 @@ package Heap;
 import java.util.*;
 
 public class LastStoneWeight{
-    public static int lastStoneWeight(int[] stones) {
+    public static int lastStoneWeightUsingAL(int[] stones) {
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < stones.length; i++) {
             ans.add(stones[i]);
@@ -18,6 +18,18 @@ public class LastStoneWeight{
         }
         if(ans.isEmpty()) return 0;
         return ans.get(0);
+    }
+    public int lastStoneWeight(int[] stones){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int ele: stones){
+            pq.add(ele);
+        }
+        while(pq.size()>1){
+            int x = pq.remove();
+            int y = pq.remove();
+            pq.add(x-y);
+        }
+        return pq.remove();
     }
     public static void main(String[] args) {
 
