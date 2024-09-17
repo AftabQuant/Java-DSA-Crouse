94589a9dfe1d5a6ab7c7c7db61c58fb6d6a4adcf
AftabQuant <iamaftabuddin123@gmail.com>
2024-09-17 10:35:48 +0530

    Something

--- Heaps And PQ/src/Heap/ImplementsMinHeap.java
+++ Heaps And PQ/src/Heap/ImplementsMinHeap.java
@@ -12,6 +12,7 @@ class minHeaps{
         arr[y] = temp;
     }
     void add(int val){
+        if(size==arr.length) throw new Error("Heaps Is Full!!!!");
         arr[size++] = val;
         upHeapify(size-1);
     }
@@ -23,23 +24,42 @@ class minHeaps{
             upHeapify(parent);
         }
     }
-    int peek() throws Error{
+    int peek(){
         if(size==0) throw new Error("Empty Heaps");
         return arr[0];
     }
-    int size(){return size;}
+    int size(){
+        return size;
+    }
     int remove() {
-        if(size==0) throw new Error("Empty Heaps");
-        
+        if(size==0) throw new Error("Empty Heaps !!!!!!!!");
+        swapFunc(0,size-1);
+        int num = arr[size-1];
+        size--;
+        downHeapify(0);
+        return num;
+    }
+    void downHeapify(int p){
+        if(p>=size) return;
+        int lc = 2*p +1;
+        int rc = 2*p +2;
+        int min = p;
+        if(lc< size && arr[lc]<arr[min]) min = lc;
+        if(rc< size && arr[rc]<arr[min]) min = rc;
+        if(p == min) return;
+        swapFunc(p,min);
+        downHeapify(min);
     }
 }
-public class ImplementsHeap {
+public class ImplementsMinHeap {
     public static void main(String[] args) {
         minHeaps pq = new minHeaps(10);
-        System.out.println(pq.peek());
         pq.add(20);
         pq.add(35);
         pq.add(12);
+        System.out.println(pq.peek());
+        pq.add(45);
+        System.out.println(pq.remove());
         System.out.println(pq.peek());
     }
 }
