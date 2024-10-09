fe9c11f86713a32d086784bc4949e4058ef9f7f7
AftabQuant <iamaftabuddin123@gmail.com>
2024-10-09 18:27:04 +0530

    Ques

---
+++ Stack/src/Java_Decode/MinAddToValid.java
@@ -0,0 +1,22 @@
+package Java_Decode;
+import java.util.Stack;
+
+public class MinAddToValid{
+    public static int minAddToMakeValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char top = s.charAt(i);
+            if(top == '(') st.push(top);
+            else {
+                if(!st.isEmpty() && st.peek()=='('){
+                    st.pop();
+                }
+                else st.push(top)
+            }
+        }
+        return st.size();
+    }
+    public static void main(String[] args) {
+
+    }
+}
