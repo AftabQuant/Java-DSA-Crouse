2e37824e99f2cbb06f5a5f269ffaae55f123d6b8
AftabQuant <iamaftabuddin123@gmail.com>
2024-10-10 08:29:28 +0530

    Sliding Window Algo

--- Sliding Window Algo/src/MaxSumSubArray.java
+++ Sliding Window Algo/src/MaxSumSubArray.java
@@ -1,5 +1,5 @@
 public class MaxSumSubArray{
-    private static int maxSum(int[] arr, int k){
+    private static int bruteForce(int[] arr, int k){
         int n = arr.length;
         int max = Integer.MIN_VALUE;
         for(int i=0; i<n-k+1; i++){
@@ -11,8 +11,23 @@ public class MaxSumSubArray{
         }
         return max;
     }
+    private static int slidingWindow(int[] arr, int k){
+        int i=0, j=k-1, sum=0;
+        for(int a=0; a<k; a++){
+            sum += arr[a];
+        }
+        i++; j++;
+        int max = Integer.MIN_VALUE;
+        while(j< arr.length){
+            sum = sum - arr[i-1] + arr[j];
+            max = Math.max(sum, max);
+            i++; j++;
+        }
+        return max;
+    }
     public static void main(String[] args) {
         int[] arr = {10, 20, 1, 3, -40, 80, 10};
-        System.out.println(maxSum(arr,3));
+        System.out.println(bruteForce(arr,3));
+        System.out.println(slidingWindow(arr,3));
     }
 }
---
+++ Sliding Window Algo/src/NumberOfSubArray.java
@@ -0,0 +1,21 @@
+public class NumberOfSubArray{
+    public static int numOfSubarrays(int[] arr, int k, int threshold) {
+        int i=0, j=k-1, sum = 0;   int ans = 0;
+        for(int a=0; a<k; a++){
+            sum += arr[a];
+        }
+        if(sum/k >= threshold) ans++;
+        i++; j++;
+        while(j < arr.length){
+            sum = sum - arr[i-1] + arr[j];
+            int avg = sum / k;
+            if(avg >= threshold) ans++;
+            i++; j++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
